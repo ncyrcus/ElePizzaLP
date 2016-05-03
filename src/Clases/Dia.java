73fef57a10abe5bb504c54553a38d9b33ff67a9d
Seba <sebastian.gaete.14@sansano.usm.cl>
2016-05-03 12:57:59 -0300

    atributos agregados a la clase dia

--- src/Clases/Dia.java
+++ src/Clases/Dia.java
@@ -4,6 +4,29 @@ package Clases;
  * Created by Neekuu on 28-04-2016.
  */
 public class Dia {
+	private double popularidad;
+	private double clientes_potenciales;
+	private int clientes_sentados;
+	private int clientes_atendidos;
+	private int clientes_que_reciben_plato;
+	private int clientes_felices;
+	private double sueldos;
+	private double ingreso;
+	private double costo;
+	private double resultado; // esto es ganancia o perdida?
+	
+	public Dia(){ //constructor
+		this.popularidad=0.0;
+		this.clientes_potenciales= 0.0;
+		this.clientes_sentados=0;
+		this.clientes_atendidos=0;
+		this.clientes_que_reciben_plato=0;
+		this.clientes_felices=0;
+		this.sueldos=0.0;
+		this.ingreso=0.0;
+		this.costo=0.0;
+		this.resultado=0.0;
+	}
     void Dia(){}
 
 }
