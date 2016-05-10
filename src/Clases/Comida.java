f8a8eb654d0c1f559c78006c6aa2bb4824be059c
Seba <sebastian.gaete.14@sansano.usm.cl>
2016-05-09 23:29:38 -0300

    Agregada clase comida y local (esto puede variar si hiciste otras funciones)

--- src/Clases/Comida.java
+++ src/Clases/Comida.java
@@ -4,4 +4,28 @@ package Clases;
  * Created by Neekuu on 28-04-2016.
  */
 public class Comida {
+
+  private double precioVenta;
+  private double costoPreparacion;
+
+  public Comida(){
+    precioVenta=40.0;
+    costoPreparacion=10.0;
+  }
+
+  public double getPrecioVenta() {
+      return precioVenta;
+  }
+
+  public double getCostoPreparacion() {
+      return costoPreparacion;
+  }
+  public void setPrecioVenta(double precioVenta){
+      this.precioVenta=precioVenta;
+  }
+
+  public void setCostoPreparacion(double costoPreparacion){
+      this.costoPreparacion=costoPreparacion;
+  }
+
 }
--- src/Clases/Local.java
+++ src/Clases/Local.java
@@ -1,7 +1,45 @@
 package Clases;
+import java.util.ArrayList;
 
-/**
- * Created by Neekuu on 28-04-2016.
- */
 public class Local {
+
+  private double pupularidad;
+  private double dineroActual;
+
+  private ArrayList<Dia> listaDias;
+  private ArrayList<Empleado> listaEmpleados;
+  private ArrayList<Mesa> listaMesas;
+
+  public Local()
+  {
+    popularidad = 0.0;
+    listaDias = new ArrayList<Dia>();
+    listaEmpleados = new ArrayList<Empleado>();
+    listaMesas = new ArrayList<Mesa>();
+  }
+
+  public void mejorarComida(Comida comidita)
+  {
+    comidita.setPrecioVenta(comidita.getPrecioVenta()+ 20.0);
+    comidita.setCostoPreparacion(comidita.getCostoPreparacion()+ 10.0);
+    System.out.println("Se ha mejorado la comida del local, ahora se vende a : " + comidita.getPrecioVenta() " pesos");
+  }
+  
+  public double getPopularidad() {
+      return popularidad;
+  }
+
+  public double getDineroActual() {
+      return dineroActual;
+  }
+
+  public void setPopularidad(double popularidad){
+      this.popularidad=popularidad;
+  }
+
+  public void setDineroActual(double dineroActual){
+      this.dineroActual=dineroActual;
+  }
+
+
 }
